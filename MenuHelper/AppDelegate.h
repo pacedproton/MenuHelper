8acfa7b4591209eca058e3df8b31f6696274ea22
Michael Alexander <malexand@scilytics.com>
2023-10-26 11:38:37 +0200

    Initial Commit

---
+++ MenuHelper/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MenuHelper
+//
+//  Created by Michael Alexander on 26.10.23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MenuHelper/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MenuHelper
+//
+//  Created by Michael Alexander on 26.10.23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
