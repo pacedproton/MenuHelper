478a41bc932525f74c434bb7fbe9f4e5351bf027
Michael Alexander <malexand@scilytics.com>
2023-10-26 14:34:06 +0200

    init ok

--- MenuHelper/AppDelegate.m
+++ MenuHelper/AppDelegate.m
@@ -1,32 +1,41 @@
-//
-//  AppDelegate.m
-//  MenuHelper
-//
-//  Created by Michael Alexander on 26.10.23.
-//
-
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-
-@end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    self.statusItem.button.title = @"📆";
+    self.statusItem.menu = [[NSMenu alloc] init];
+    [self.statusItem.menu addItemWithTitle:@"Insert Date" action:@selector(insertDate:) keyEquivalent:@""];
+    [self.statusItem.menu addItem:[NSMenuItem separatorItem]];  // separator
+    [self.statusItem.menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)insertDate:(id)sender {
+    // Logic for inserting the current date where the cursor is located.
+    // 1. Get the current date as a string
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateStyle:NSDateFormatterShortStyle];
+        NSString *currentDate = [dateFormatter stringFromDate:[NSDate date]];
+        
+        // 2. Copy to clipboard
+        NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+        [pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
+        [pasteboard setString:currentDate forType:NSStringPboardType];
+        
+        // 3. Simulate a 'paste' command
+        // You need to ensure your app has accessibility permissions to simulate key presses.
+        CGEventSourceRef source = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+        CGEventRef pasteKeyDown = CGEventCreateKeyboardEvent(source, (CGKeyCode)9, YES); // 'v' key down
+        CGEventRef pasteKeyUp = CGEventCreateKeyboardEvent(source, (CGKeyCode)9, NO);   // 'v' key up
+        CGEventSetFlags(pasteKeyDown, kCGEventFlagMaskCommand); // Add Command key flag to the event (Command + v)
+        CGEventSetFlags(pasteKeyUp, kCGEventFlagMaskCommand);   // Add Command key flag to the event (Command + v)
+        CGEventPost(kCGHIDEventTap, pasteKeyDown);
+        CGEventPost(kCGHIDEventTap, pasteKeyUp);
+        CFRelease(pasteKeyUp);
+        CFRelease(pasteKeyDown);
+        CFRelease(source);
+    
 }
-
-
-- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
-    return YES;
-}
-
 
 @end
